efc5008bb9ae1dcd444a195942c75037418f72a5
Bright Issah <bissah@mra.mw>
2022-08-02 16:56:35 +0200

    cleaning ui

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -103,7 +103,9 @@
         </div>
       </v-app-bar>
     </nav>
-    <router-view />
+    <transition name="page" mode="out-in">
+      <router-view/>
+    </transition>  
     <Footer></Footer>
   </div>
 </template>
@@ -169,3 +171,13 @@ export default {
   }
 }
 </script>
+
+<style>
+.page-enter-active, .page-leave-active {
+  transition: opacity 1s, transform 1s;
+}
+.page-enter, .page-leave-to {
+  opacity: 0;
+  transform: translateX(-2%);
+}
+</style>
